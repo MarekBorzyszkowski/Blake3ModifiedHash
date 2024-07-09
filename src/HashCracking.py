2c61cd83f7087a092db444178d7b87ff6be258a4
Marek Borzyszkowski <marek.borzyszkowski@pg.edu.pl>
2024-07-09 14:58:44 +0200

    Adding custom number of elements

--- src/HashCracking.py
+++ src/HashCracking.py
@@ -18,11 +18,11 @@ def compare_hash(expected_hash, actual_hash):
     return 1
 
 
-@jit(nb.types.UniTuple(nb.uint16, 2)(nb.uint16[:]), nopython=True, parallel=True)
-def crack_hash(expected_hash):
+@jit(nb.types.UniTuple(nb.uint16, 2)(nb.uint16, nb.uint16[:]), nopython=True, parallel=True)
+def crack_hash(entry_message_length, expected_hash):
     number_of_elements = len(allowed_val)
     combinations = []
-    result = [np.uint16(0), np.uint16(0)]
+    result = [np.uint16(0) for _ in range(entry_message_length)]
     for a in allowed_val:
         for b in allowed_val:
             combinations.append(np.array([a, b], dtype=np.uint16))
@@ -37,8 +37,7 @@ def convert_cracked_hash_to_string(initial_combination):
     return ''.join([allowed_val_to_letters[element] for element in initial_combination])
 
 
-
-original_combination = crack_hash(
+original_combination = crack_hash(2,
     np.array([np.uint16(0x290D), np.uint16(0x8E30), np.uint16(0xA7F7), np.uint16(0x58DE),
               np.uint16(0x023C), np.uint16(0x9C74), np.uint16(0x6233),
               np.uint16(0x631D)], dtype=np.uint16), )
