a2b2a8a177b32fb31fa756738ebb668866b1f973
Marek Borzyszkowski <marek.borzyszkowski@pg.edu.pl>
2024-07-09 14:45:21 +0200

    working hash cracking for 2 elements

---
+++ src/HashCracking.py
@@ -0,0 +1,47 @@
+import itertools
+
+import numpy as np
+import numba as nb
+from src.HashFunc import blake3_hash
+from numba import jit, prange
+
+allowed_letters = 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890!@#$%^&*-_=+([{<)]}>\'";:?,.\\/|'
+allowed_val_to_letters = {np.uint16(ord(character)): character for character in allowed_letters}
+allowed_val = np.array([np.uint16(ord(character)) for character in allowed_letters])
+
+
+@jit(nb.uint16(nb.uint16[:], nb.uint16[:]), nopython=True)
+def compare_hash(expected_hash, actual_hash):
+    for i in range(len(expected_hash)):
+        if expected_hash[i] != actual_hash[i]:
+            return 0
+    return 1
+
+
+@jit(nb.types.UniTuple(nb.uint16, 2)(nb.uint16[:]), nopython=True, parallel=True)
+def crack_hash(expected_hash):
+    number_of_elements = len(allowed_val)
+    combinations = []
+    result = [np.uint16(0), np.uint16(0)]
+    for a in allowed_val:
+        for b in allowed_val:
+            combinations.append(np.array([a, b], dtype=np.uint16))
+    for i in prange(len(combinations)):
+        equal = compare_hash(expected_hash, blake3_hash(combinations[i]))
+        result[0] += equal * combinations[i][0]
+        result[1] += equal * combinations[i][1]
+    return result[0], result[1]
+
+
+def convert_cracked_hash_to_string(initial_combination):
+    return ''.join([allowed_val_to_letters[element] for element in initial_combination])
+
+
+
+original_combination = crack_hash(
+    np.array([np.uint16(0x290D), np.uint16(0x8E30), np.uint16(0xA7F7), np.uint16(0x58DE),
+              np.uint16(0x023C), np.uint16(0x9C74), np.uint16(0x6233),
+              np.uint16(0x631D)], dtype=np.uint16), )
+print(original_combination)
+original_string = convert_cracked_hash_to_string(original_combination)
+print(original_string)
