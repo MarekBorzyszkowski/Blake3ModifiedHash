da06fda9899b8f2a884ff288fa0acb49eceb2e05
Marek Borzyszkowski <marek.borzyszkowski@pg.edu.pl>
2024-07-10 15:42:55 +0200

    working CUDA permutations

--- src/CUDA/Permutations_CUDA.py
+++ src/CUDA/Permutations_CUDA.py
@@ -10,7 +10,7 @@ S_PERMUTATIONS = np.array([5, 8, 0, 2, 6, 11, 1, 4, 15, 12, 3, 9, 10, 7, 13, 14]
 
 @cuda.jit(nb.uint32(nb.uint32, nb.uint32), device=True)
 def rotl(n, d):
-    return operator.iand(operator.lshift(n, d) | (operator.rshift(n,SHORT_SIZE - d)), 0xFFFF)
+    return ((n << d) | (n >> SHORT_SIZE - d)) & 0xFFFF
 
 
 @cuda.jit(nb.void(nb.uint32, nb.uint32, nb.uint32))
@@ -18,19 +18,32 @@ def rotl_test(n, d, output):
     output[0] = rotl(n[0], d[0])
 
 
-# @jit(nb.types.UniTuple(nb.uint32, 4)(nb.uint32, nb.uint32, nb.uint32, nb.uint32, nb.uint32, nb.uint32), nopython=True)
-# def G_function(a, b, c, d, x, y):
-#     a = (a + b + x) & 0xFFFF
-#     d = rotl((d ^ a), 3)
-#     c = (c + d) & 0xFFFF
-#     b = rotl((b ^ c), 11)
-#     a = (a + b + y) & 0xFFFF
-#     d = rotl((d ^ a), 2)
-#     c = (c + d) & 0xFFFF
-#     b = rotl((b ^ c), 5)
-#     return a, b, c, d
-#
-#
-# @jit(nb.uint32[:](nb.uint32[:]), nopython=True)
-# def permute_m_by_s(m):
-#     return np.array([m[S_PERMUTATIONS[i]] for i in range(SHORT_SIZE)])
+@cuda.jit(nb.types.UniTuple(nb.uint32, 4)(nb.uint32, nb.uint32, nb.uint32, nb.uint32, nb.uint32, nb.uint32),
+          device=True)
+def G_function(a, b, c, d, x, y):
+    a = (a + b + x) & 0xFFFF
+    d = rotl((d ^ a), 3)
+    c = (c + d) & 0xFFFF
+    b = rotl((b ^ c), 11)
+    a = (a + b + y) & 0xFFFF
+    d = rotl((d ^ a), 2)
+    c = (c + d) & 0xFFFF
+    b = rotl((b ^ c), 5)
+    return a, b, c, d
+
+
+@cuda.jit(nb.void(nb.uint32[:]))
+def G_function_test(array):
+    array[0], array[1], array[2], array[3] = G_function(array[0], array[1], array[2], array[3], array[4], array[5])
+
+
+@cuda.jit(nb.uint32[:](nb.uint32[:]), device=True)
+def permute_m_by_s(m):
+    return np.array([m[S_PERMUTATIONS[i]] for i in range(SHORT_SIZE)])
+
+
+@cuda.jit(nb.void(nb.uint32[:]))
+def permute_m_by_s_test(m):
+    results = permute_m_by_s(m)
+    for i in range(len(m)):
+        m[i] = results[i]
--- test/CUDA/Permutations_CUDA_TEST.py
+++ test/CUDA/Permutations_CUDA_TEST.py
@@ -3,7 +3,7 @@ import unittest
 import numpy as np
 from numba import cuda
 
-from src.CUDA.Permutations_CUDA import rotl_test
+from src.CUDA.Permutations_CUDA import rotl_test, G_function_test, permute_m_by_s_test
 
 THREADS_PER_BLOCK = 128
 BLOCKS_PER_GRID = 16
@@ -17,19 +17,52 @@ class MyTestCase(unittest.TestCase):
         expected = 2
         rotl_test[1, 1](number, shift, cuda_result)
         result = cuda_result.copy_to_host()
-        self.assertEqual(expected, result[0])  # add assertion here
+        self.assertEqual(expected, result[0])
 
     def test_rol_shift_with_one_going_to_beginning(self):
-        number = np.uint16(32769)
-        shift = 1
-        expected = np.uint16(3)
-        self.assertEqual(expected, rotl_test(number, shift))  # add assertion here
+        number = np.array([32769], dtype=np.int32)
+        shift = np.array([1], dtype=np.int32)
+        cuda_result = cuda.device_array([1], dtype=np.int32)
+        expected = 3
+        rotl_test[1, 1](number, shift, cuda_result)
+        result = cuda_result.copy_to_host()
+        self.assertEqual(expected, result[0])
 
     def test_rol_number_length_rol(self):
-        number = np.uint16(1234)
-        shift = 16
-        expected = np.uint16(1234)
-        self.assertEqual(expected, rotl_test(number, shift))  # add assertion here
+        number = np.array([1234], dtype=np.int32)
+        shift = np.array([16], dtype=np.int32)
+        cuda_result = cuda.device_array([1], dtype=np.int32)
+        expected = 1234
+        rotl_test[1, 1](number, shift, cuda_result)
+        result = cuda_result.copy_to_host()
+        self.assertEqual(expected, result[0])
+
+    def test_G_function(self):
+        v00 = 0
+        v10 = 0
+        v20 = 0x03F4
+        v30 = 0
+        m0 = 0x0001
+        m1 = 0x0203
+        cuda_array = cuda.to_device(np.array([v00, v10, v20, v30, m0, m1], dtype=np.int32))
+        G_function_test[1, 1](cuda_array)
+        result = cuda_array.copy_to_host()
+        self.assertEqual(0xE223, result[0])
+        self.assertEqual(0x968D, result[1])
+        self.assertEqual(0x8CAB, result[2])
+        self.assertEqual(0x88AF, result[3])
+
+    def test_permute_m_by_s(self):
+        m = np.array([np.uint16(((2 * i) << 8) + (2 * i + 1)) for i in range(16)])
+        expected = np.array([np.uint16(0x0A0B), np.uint16(0x1011), np.uint16(0x0001), np.uint16(0x0405),
+                             np.uint16(0x0C0D), np.uint16(0x1617), np.uint16(0x0203), np.uint16(0x0809),
+                             np.uint16(0x1E1F), np.uint16(0x1819), np.uint16(0x0607), np.uint16(0x1213),
+                             np.uint16(0x1415), np.uint16(0x0E0F), np.uint16(0x1A1B), np.uint16(0x1C1D)])
+        cuda_array = cuda.to_device(m)
+        permute_m_by_s_test[1, 1](cuda_array)
+        result = cuda_array.copy_to_host()
+        for i in range(len(m)):
+            self.assertEqual(expected[i], result[i])
 
 
 if __name__ == '__main__':
