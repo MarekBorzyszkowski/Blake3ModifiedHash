6183c12a0215723ae69b1ab60863ab13e046348d
Marek Borzyszkowski <marek.borzyszkowski@pg.edu.pl>
2024-07-10 17:55:50 +0200

    removal of signature

--- src/CUDA/Permutations_CUDA.py
+++ src/CUDA/Permutations_CUDA.py
@@ -6,13 +6,12 @@ SHORT_SIZE = 16
 S_PERMUTATIONS = np.array([5, 8, 0, 2, 6, 11, 1, 4, 15, 12, 3, 9, 10, 7, 13, 14])
 
 
-@cuda.jit(nb.void(nb.uint32, nb.uint32), device=True)
+@cuda.jit(device=True)
 def rotl(n, d):
     n = ((n << d) | (n >> SHORT_SIZE - d)) & 0xFFFF
 
 
-@cuda.jit(nb.void(nb.uint32, nb.uint32, nb.uint32, nb.uint32, nb.uint32, nb.uint32),
-          device=True)
+@cuda.jit(device=True)
 def G_function(a, b, c, d, x, y):
     a = (a + b + x) & 0xFFFF
     rotl((d ^ a), 3)
@@ -24,7 +23,7 @@ def G_function(a, b, c, d, x, y):
     rotl((b ^ c), 5)
 
 
-@cuda.jit(nb.void(nb.uint32[:]), device=True)
+@cuda.jit(device=True)
 def permute_m_by_s(m):
     results = np.array([m[S_PERMUTATIONS[i]] for i in range(SHORT_SIZE)], dtype=np.uint32)
     for i in range(SHORT_SIZE):
