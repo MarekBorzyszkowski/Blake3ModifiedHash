b50511ab5991283c4eb0b8f5299017e936e4079b
Marek Borzyszkowski <marek.borzyszkowski@pg.edu.pl>
2024-07-10 17:59:20 +0200

    removal of signature

--- src/CUDA/HashFunc_CUDA.py
+++ src/CUDA/HashFunc_CUDA.py
@@ -5,52 +5,49 @@ from numba import cuda
 from Permutations_CUDA import G_function, permute_m_by_s
 
 
-@cuda.jit(nb.uint32[:](nb.uint32[:], nb.uint32[:]), device=True)
+@cuda.jit(device=True)
 def vertical_permutation(v, m):
     G_function(v[0], v[4], v[8], v[12], m[0], m[1])
     G_function(v[1], v[5], v[9], v[13], m[2], m[3])
     G_function(v[2], v[6], v[10], v[14], m[4], m[5])
     G_function(v[3], v[7], v[11], v[15], m[6], m[7])
-    return v
 
 
-@cuda.jit(nb.uint32[:](nb.uint32[:], nb.uint32[:]), device=True)
+@cuda.jit(device=True)
 def diagonal_permutation(v, m):
     G_function(v[0], v[5], v[10], v[15], m[8], m[9])
     G_function(v[1], v[6], v[11], v[12], m[10], m[11])
     G_function(v[2], v[7], v[8], v[13], m[12], m[13])
     G_function(v[3], v[4], v[9], v[14], m[14], m[15])
-    return v
 
 
-@cuda.jit(nb.types.UniTuple(nb.uint32[:], 2)(nb.uint32[:], nb.uint32[:]), device=True)
+@cuda.jit(device=True)
 def make_round(v, m):
-    v = vertical_permutation(v, m)
-    v = diagonal_permutation(v, m)
+    vertical_permutation(v, m)
+    diagonal_permutation(v, m)
     permute_m_by_s(m)
-    return v, m
 
 
-@cuda.jit(nb.uint32[:](nb.uint32[:], nb.uint32[:], nb.uint32), device=True)
+@cuda.jit(device=True)
 def hash_block(w, m, block_number):
     v = np.array([w[0], w[1], w[2], w[3],
                   w[4], w[5], w[6], w[7],
                   np.uint32(0x03F4), np.uint32(0x774C), np.uint32(0x5690), np.uint32(0xC878),
                   np.uint32(0), np.uint32(block_number), np.uint32(0), np.uint32(0)])
     for i in range(6):
-        v, m = make_round(v, m)
+        make_round(v, m)
     return np.array([w[0] ^ v[0] ^ v[8], w[1] ^ v[1] ^ v[9], w[2] ^ v[2] ^ v[10], w[3] ^ v[3] ^ v[11],
                      w[4] ^ v[4] ^ v[12], w[5] ^ v[5] ^ v[13], w[6] ^ v[6] ^ v[14], w[7] ^ v[7] ^ v[15]],
                     dtype=np.uint32)
 
 
-@cuda.jit(nb.uint32[:](nb.uint32[:]), device=True)
+@cuda.jit(device=True)
 def merge_bytes(block_as_bytes):
     return np.array([np.uint32((block_as_bytes[2 * i] << 8) + block_as_bytes[2 * i + 1])
                      for i in range(len(block_as_bytes) // 2)])
 
 
-@cuda.jit(nb.uint32[:](nb.uint32[:]), device=True)
+@cuda.jit(device=True)
 def fill_blocks(block):
     number_of_elements = block.shape[0]
     number_of_missing_elements = 32 - number_of_elements % 32
@@ -72,7 +69,7 @@ def fill_blocks(block):
     return block
 
 
-@cuda.jit(nb.uint32[:](nb.uint32[:]), device=True)
+@cuda.jit(device=True)
 def blake3_hash(block_of_bytes):
     block_of_bytes = fill_blocks(block_of_bytes)
     block_of_words = merge_bytes(block_of_bytes)
